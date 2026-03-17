58d08ba2bec1bb86451c4344bbb28b80b4ceb43b
Weso <mohamed.hosam.eldeeen@gmail.com>
2026-03-17 14:32:47 +0200

    Add checked-task list and move-on-click
    Add a new checked-task section and styles, and update JavaScript to support moving tasks to the checked list on click. HTML: insert <section class="checked-task"><ul id="checkedtask"></ul></section>. JS: grab checkedtask element, create list items (li1) with an end-span remove button, apply alternating even/odd row classes, append new tasks to taskList, add click handler to move a task to the checkedtask list and a click on the span to remove an item. CSS: add layout and styling for .checked-task, #checkedtask, li hover, and reuse even/odd backgrounds.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var Add = document.getElementById("add")
 var Task = document.getElementById("task")
 var taskList = document.getElementById("taskList")
+var checkedtask = document.getElementById("checkedtask")
 
 Add.onclick = function() {
     // console.log(Task.value)
@@ -10,26 +11,41 @@ Add.onclick = function() {
     }
     else{
         var taskText = Task.value;
-        var li = document.createElement("li");
+        var li1 = document.createElement("li");
         var span = document.createElement("span");
-
-        li.appendChild(document.createTextNode(taskText));
+        
+        li1.appendChild(document.createTextNode(taskText));
         span.textContent = "×";
-        span.classList.add("end-span");
+        span.classList.add("end-span"); 
+        
+        li1.appendChild(span);
+        
+        if (taskList.children.length % 2 === 0) {
+            li1.classList.add("even");
+        } else {
+            li1.classList.add("odd");
+        }
+        
+        taskList.appendChild(li1);
+        Task.value = "";
+
+        // adding seperate section for checked tasks when clicking on the task in the origrinal list
+        li1.onclick = function() {
+            var li2 = document.createElement("li");
+            li2.appendChild(document.createTextNode(li1.value));
+
+            if (checkedtask.children.length % 2 === 0) {
+            li2.classList.add("even");
+            } else {
+                li2.classList.add("odd");
+            }
+
+            checkedtask.appendChild(li2)
+            this.parentElement.remove();
+        }
 
         span.onclick = function() {
             this.parentElement.remove();
-        };
-
-        li.appendChild(span);
-
-        if (taskList.children.length % 2 === 0) {
-            li.classList.add("even");
-        } else {
-            li.classList.add("odd");
         }
-
-        taskList.appendChild(li);
-        Task.value = "";
     }
 } 
